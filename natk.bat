daba02fd1584000cc5f7107dac5ada1c3032525c
Wikiwiki007 <rayanebilal@outlook.com>
2015-07-13 12:29:57 +0200

    Whois
    Added the whois function

--- natk.bat
+++ natk.bat
@@ -1,11 +1,11 @@
 @echo off 
 color F0
 title NetworkAnalysisToolkit
-
+:start
 echo WELCOME TO NATK!
 
 pause
-:start
+
 echo Press 1 for ipconfig
 echo Press 2 for whois
 echo Press 3 for netstats
@@ -26,10 +26,10 @@ if %car% == 3 goto netstats
 		echo Loading...
 		ping localhost -n 2 >nul
 		cls
-
+		:ip
 		echo WELOCME TO THE IPCONFIG MODULE!
 		pause
-		:ip
+		
 		echo Type all for full configuration information.
 		echo Type Flush to Purge the DNS Resolver cache.
 		echo Type register to  Refresh all DHCP leases and re-register DNS names.
@@ -94,3 +94,44 @@ if %car% == 3 goto netstats
 		if %var% == s goto start
 		if %var% == e goto EOF
 			
+			
+			
+			
+			
+			
+			
+			:whois
+			echo Loading.
+        ping localhost -n 2 >nul
+		cls
+		echo Loading..
+		ping localhost -n 2 >nul
+		cls
+		echo Loading...
+		ping localhost -n 2 >nul
+		cls
+		:is
+	    echo WELCOME TO THE WHOIS MODULE!
+		
+		echo Press 9 to start whois
+		echo Press 8 to go to the main menu
+		set /p whois=
+		if %whois% == 9 start www.whois.net & goto who
+		if %whois% == 8 cls & goto start 
+		if %whois% NEQ 9 cls & goto is
+		if %whois% NEQ 8 cls & goto is
+		
+		:who
+		
+	
+		echo done!
+	    echo Press s to go to the menu
+		echo Press e to close the window
+		set /p var=
+		if %var% == s goto start
+		if %var% == e goto EOF
+		
+		
+		
+			
+		
